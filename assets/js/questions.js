ca458ac538418d2c44ff6454150c4a06560de01e
paulpaulchan <116852901+ngwingyan@users.noreply.github.com>
2023-01-29 04:54:22 +0000

    update questions javascript

---
+++ assets/js/questions.js
@@ -0,0 +1,85 @@
+// questions from W3 schools and tutorialspoint//
+var quizQuestions = [
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    choices: [
+      "A. <javascript>",
+      "B. <js>",
+      "C. <scripting>",
+      "D. <script>"
+    ],
+    correctAnswer: "D. <script>"
+  },
+  {
+    question: "How to write an IF statement in JavaScript?",
+    choices: [
+      "A. if i == 5 then",
+      "B. if i = 5 then",
+      "C. if (i == 5)",
+      "D. if i = 5"
+    ],
+    correctAnswer: "C. if (i == 5)"
+  },
+  {
+    question: "How does a FOR loop start?",
+    choices: [
+      "A. for (i <= 5; i++)",
+      "B. for i = 1 to 5", 
+      "C. for (i = 0; i <= 5)", 
+      "D. for (i = 0; i <= 5; i++)"
+    ],
+    correctAnswer: "D. for (i = 0; i <= 5; i++)"
+  },
+  {
+    question: "How do you round the number 7.25, to the nearest integer?",
+    choices: [
+      "A. Math.rnd(7.25)",
+      "B. rnd(7.25)",
+      "C. round(7.25)",
+      "D. Math.round(7.25)"
+    ],
+    correctAnswer: "D. Math.round(7.25)"
+  },
+  {
+    question:
+      "Which event occurs when the user clicks on an HTML element?",
+    choices: [
+      "A. onmouseover",
+      "B. onmouseclick",
+      "C. onclick",
+      "D. onchange"
+    ],
+    correctAnswer: "C. onclick"
+  },
+  {
+    question: "What is the correct way to write a JavaScript array?",
+    choices: [
+      "A. quotes", 
+      "B. curly brackets", 
+      "C. parentheses", 
+      "D. square brackets"],
+    correctAnswer: "C. parentheses"
+  },
+  {
+    question:
+      "Which operator is used to assign a value to a variable?",
+    choices: [
+      "A. -", 
+      "B. *", 
+      "C. x", 
+      "D. ="
+    ],
+    correctAnswer: "D. ="
+  },
+  {
+    question:
+      "Which of the following function of Array object sorts the elements of an array?",
+    choices: [
+      "A. toSource()", 
+      "B. sort ()", 
+      "C. toString()", 
+      "D. unshift()"
+    ],
+    correctAnswer: "B. sort ()"
+  },
+];
